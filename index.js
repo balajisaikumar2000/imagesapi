9af355bc68fd7c28508cc1f9bb4b4ce6bff5389c
balajisaikumar200 <balajisaikumar3@gmail.com>
2022-09-24 09:25:43 +0530

    asijn

--- index.js
+++ index.js
@@ -14,12 +14,11 @@ app.use(bodyParser.json());
 
 mongoose
   .connect(
-    "mongodb+srv://sahul:FEmG5ZQcSav3DndD@cluster0.alht5.mongodb.net/imageData?retryWrites=true&w=majority",
+    "mongodb+srv://balajisaikumar:Balajisaikumar%402000@cluster0.k8khhpl.mongodb.net/?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("connected successfully"))
   .catch((err) => console.log("it has an error", err));
-
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -33,7 +32,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post("/", upload.single("testImage"), (req, res) => {
-  const saveImage =  imageModel({
+  const saveImage = imageModel({
     name: req.body.name,
     img: {
       data: fs.readFileSync("uploads/" + req.file.filename),
@@ -48,14 +47,13 @@ app.post("/", upload.single("testImage"), (req, res) => {
     .catch((err) => {
       console.log(err, "error has occur");
     });
-    res.send('image is saved')
+  res.send("image is saved");
 });
 
-
-app.get('/',async (req,res)=>{
-  const allData = await imageModel.find()
-  res.json(allData)
-})
+app.get("/", async (req, res) => {
+  const allData = await imageModel.find();
+  res.json(allData);
+});
 
 app.listen(port, () => {
   console.log("server running successfully");
